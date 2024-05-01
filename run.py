f9d8a828e8ed1aa661f7e4c5dcb4b912fe799466
Natanael Sosa <natanaelsosa@gmail.com>
2024-04-30 21:42:49 -0400

    post and get

---
+++ run.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contact.db'
+db = SQLAlchemy(app)
+
+#crear Modelo de base de datos
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True) #Llave primaria
+    name = db.Column(db.String(80), nullable=False) #
+    email = db.Column(db.String(120), unique=True)   #Correo electronico con restric        
+    phone = db.Column(db.String(80), nullable=False)  #Telefono
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "phone": self.phone
+        }
+        
+#Crear las tablas
+with app.app_context():
+    db.create_all()
+ 
+ 
+ # Crear rutas   
+@app.route("/contacts", methods=["GET"])
+def get_contact():
+    contacts = Contact.query.all()
+    list_contact = []
+    for cantact in contacts:
+        list_contact.append(cantact.serialize())
+    return jsonify({'contacs':[contact.serialize() for contact in contacts]})
+    
+@app.route("/contacts", methods=["POST"])
+def create_contact():
+    return 'Crear contacts'
+    
+if __name__ == "__main__":
+    app.run(debug=True)
